3045244dfec98457853e0bf9a66fe82a17a9d501
Palpa01 <00001077441265sp@al.educacao.sp.gov.br>
2025-09-05 21:26:20 -0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Aumentar fonte
+function aumentarFonte() {
+  document.body.style.fontSize = "larger";
+}
+
+// ScrollReveal
+ScrollReveal().reveal('.section', {
+  duration: 1000,
+  origin: 'bottom',
+  distance: '50px'
+});
